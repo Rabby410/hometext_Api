97662c9b240350260ae959591b5f080fbe3f5ac4
Rabby <rabby410@gmail.com>
2023-07-01 17:55:18 +0600

    category table bug solving Migration

--- database/migrations/2023_03_17_200200_create_categories_table.php
+++ database/migrations/2023_03_17_200200_create_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sub_categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->string('slug')->nullable();
@@ -19,8 +19,7 @@ return new class extends Migration
             $table->tinyInteger('status')->nullable();
             $table->string('description')->nullable();
             $table->string('photo')->nullable();
-            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete()->nullable();
-            $table->foreignId('category_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete()->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete()->nullable()   ;
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sub_categories');
+        Schema::dropIfExists('categories');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -81,28 +81,6 @@ Route::group(['middleware' => ['auth:sanctum', 'auth:admin']], function () {
 
 
 Route::group(['middleware' => ['auth:sanctum', 'auth:sales_manager']], function () {
-    //    Route::post('logout', [AuthController::class, 'logout']);
-    //    Route::get('get-attribute-list', [AttributeController::class, 'get_attribute_list']);
-    //    Route::get('get-supplier-list', [SupplierController::class, 'get_provider_list']);
-    //    Route::get('get-country-list', [CountryController::class, 'get_country_list']);
-    //    Route::get('get-brand-list', [BrandController::class, 'get_brand_list']);
-    //    Route::get('get-category-list', [CategoryController::class, 'get_category_list']);
-    //    Route::get('get-shop-list', [ShopController::class, 'get_shop_list']);
-    //    Route::apiResource('sales-manager', SalesManagerController::class);
-    //    Route::get('get-sub-category-list/{category_id}', [SubCategoryController::class, 'get_sub_category_list']);
-    //    Route::post('product-photo-upload/{id}', [ProductPhotoController::class, 'store']);
-    //    Route::apiResource('category', CategoryController::class);
-    //    Route::apiResource('sub-category', SubCategoryController::class);
-    //    Route::apiResource('brand', BrandController::class);
-    //    Route::apiResource('supplier', SupplierController::class);
-    //    Route::apiResource('attribute', AttributeController::class);
-    //    Route::apiResource('attribute-value', AttributeValueController::class);
-    //    Route::apiResource('product', ProductController::class);
-    //    Route::apiResource('photo', ProductPhotoController::class);
-    //    Route::apiResource('shop', ShopController::class);
-    //    Route::apiResource('customer', CustomerController::class);
-    //    Route::apiResource('order', OrderController::class);
-    //    Route::get('get-payment-methods', [PaymentMethodController::class, 'index']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('sales-manager', SalesManagerController::class);
     Route::get('get-attribute-list', [AttributeController::class, 'get_attribute_list']);
